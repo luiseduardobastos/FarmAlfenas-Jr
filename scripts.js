f38d30cf335195b973c6731e11d4763b7b6bc41f
luiseduardobastos5446 <luiseduardobastos5446@gmail.com>
2024-04-28 16:57:07 -0300

    Criação da Home - V8

--- scripts.js
+++ scripts.js
@@ -1,3 +1,29 @@
+const btnAbrirNav = document.getElementById("btn-abrir-nav");
+const btnFecharNav = document.getElementById("btn-fechar-nav");
+const navCel = document.getElementById("nav-cel");
+
+// Abrir a navCel
+btnAbrirNav.addEventListener("click", function () {
+  navCel.style.display = "flex";
+  btnAbrirNav.style.display = "none";
+  btnFecharNav.style.display = "flex";
+});
+
+// Fechar a navCel
+navCel.addEventListener("click", function (event) {
+  if (event.target === navCel) {
+    navCel.style.display = "none";
+    btnAbrirNav.style.display = "flex";
+    btnFecharNav.style.display = "none";
+  }
+});
+
+btnFecharNav.addEventListener("click", function () {
+  navCel.style.display = "none";
+  btnAbrirNav.style.display = "flex";
+  btnFecharNav.style.display = "none";
+});
+
 const btnAbrirContato = document.getElementById("btn-abrir-contato");
 const btnFecharContato = document.getElementById("btn-fechar-contato");
 const contatoModal = document.getElementById("contato-modal");
